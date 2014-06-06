e007d861385a46c1635961e2ce272f8d838efe18
Gerard Cote <grcote@gmail.com>
2014-06-06 16:06:07 -0600

    Use the verifier from the app so we get the more secure version
    Signed-off-by: Mike Gehard <mike.gehard@gmail.com>

--- app/models/secure_tokens.rb
+++ app/models/secure_tokens.rb
@@ -3,12 +3,12 @@ class SecureTokens
   end
 
   def self.password_reset_for(user)
-    verifier = ActiveSupport::MessageVerifier.new(ENV['VERIFICATION_TOKEN'], serializer: YAML)
+    verifier = Rails.application.message_verifier(:password_reset)
     verifier.generate({:user_id => user.id, expiration: 2.hours.from_now})
   end
 
   def self.verify_password_reset_token(token)
-    verifier = ActiveSupport::MessageVerifier.new(ENV['VERIFICATION_TOKEN'], serializer: YAML)
+    verifier = Rails.application.message_verifier(:password_reset)
     begin
       decrypted_hash = verifier.verify(token)
     rescue ActiveSupport::MessageVerifier::InvalidSignature
